0fff7c912f3fb0f710ea52cf886a237d948ab804
Samuel Fox <samf1596@gmail.com>
2018-07-13 20:08:52 -0400

    see what happens

--- bot.py
+++ bot.py
@@ -1,13 +1,27 @@
 import json
 import os
-
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
 import requests
 from flask import Flask, request
 
 app = Flask(__name__)
 
+def send_message(msg):
+  url  = 'https://api.groupme.com/v3/bots/post'
+
+  data = {
+          'bot_id' : os.getenv('GROUPME_BOT_ID'),
+          'text'   : msg,
+         }
+  request = Request(url, urlencode(data).encode())
+  json = urlopen(request).read().decode()
+
 @app.route('/', methods=['POST'])
 def hook():
-    data = request.json()
-    print(data)
+      # We don't want to reply to ourselves!
+    if data['name'] != 'Yelp':
+        msg = '{}, you sent "{}".'.format(data['name'], data['text'])
+        send_message(msg)
+
     return "ok", 200

bd85631d25a61ef0fcd248c63f6d26212cf53bc5
Samuel Fox <samf1596@gmail.com>
2018-07-16 19:57:07 -0400

    update

--- bot.py
+++ bot.py
@@ -45,6 +45,7 @@ def hook():
 
     if data['name'] != 'Yelp' and ("food" in data['text'].lower()):
         send_message("Where would like like to search for food? (city/zip/state/combo)")
+        bot == True
         while bot==True:
             res = get_message()
             location = res["messages"][0]["text"]
